ee2631ff9ce6ded6919321b63b54011fea5dab06
Chun-zen <chunzen1998@gmail.com>
2019-09-17 02:10:35 -0300

    Selenium Test Added

--- src/test/java/com/example/demo/BackendTests.java
+++ src/test/java/com/example/demo/BackendTests.java
@@ -8,7 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
